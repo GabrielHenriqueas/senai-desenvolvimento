53f7c29cb1edaec89970765fa60d65628fa78d13
Gabriel Henrique Silva <gabriel.silva@email.com>
2023-04-09 20:09:11 -0300

    dia 09.04.2023

--- 2023-1S-1D/Backend/listaExercicio03/fibonacci/Program.cs
+++ 2023-1S-1D/Backend/listaExercicio03/fibonacci/Program.cs
@@ -1,2 +1,22 @@
 ﻿//  Pesquise como funciona o algoritmo Fibonacci.
 //  Faça um programa que gere a série até que o valor seja maior que 500.
+
+int a = 1;
+int b = 2;
+int c = a += b;
+
+a += b = c;
+
+Console.WriteLine($"{c}");
+
+
+// for(int cont = 1 ; cont <= 10 ; cont++)
+// {
+//     a += b = c;
+
+//     Console.WriteLine($"{c}");
+    
+//     c += b = a;
+//     c = b;
+    
+// }
